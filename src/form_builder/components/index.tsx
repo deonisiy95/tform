20085b69c33987a4239de7389bece82d7eee3492
denis <deonisiy9572@gmail.com>
2023-01-21 22:45:10 +0500

    Add save button to form page

--- src/form_builder/components/index.tsx
+++ src/form_builder/components/index.tsx
@@ -3,6 +3,7 @@ import style from './style.less';
 import {MenuItem} from 'src/form_builder/components/Menu/Item';
 import {TTypeControl} from 'src/form_builder/@types/formBuilder';
 import cn from 'classnames';
+import Button from 'UI/Button';
 
 interface IProps {
   items: TTypeControl[];
@@ -14,13 +15,18 @@ interface IProps {
 export const FormBuilder: FC<IProps> = ({items, form, options, onAddControl}) => {
   return (
     <div className={style.container}>
-      <div className={style.menu}>
-        {items.map((item, index) => (
-          <MenuItem key={index} title={l10n(item)} type={item} onClick={onAddControl} />
-        ))}
+      <div className={style.form}>
+        <div className={style.menu}>
+          {items.map((item, index) => (
+            <MenuItem key={index} title={l10n(item)} type={item} onClick={onAddControl} />
+          ))}
+        </div>
+        <div className={cn(style.content, 'scroll')}>{form}</div>
+        <div className={cn(style.settings, 'scroll')}>{options}</div>
       </div>
-      <div className={cn(style.content, 'scroll')}>{form}</div>
-      <div className={style.settings}>{options}</div>
+      <div className={style.controls}>
+        <Button className={style.buttonSave} size={'sm'} onClick={() => {}}>{l10n('save')}</Button>
+      </div>
     </div>
   );
 };
